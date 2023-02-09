23241c79dbc22e6b5e53a2a5c3e5f27fdb67d6cc
grynevych89 <grynevych89@gmail.com>
2023-02-09 19:50:46 +0200

    done dz 4

---
+++ HomeWork/hw2/dz4/app.js
@@ -0,0 +1,21 @@
+let askHoues;
+
+do {
+    askHoues = prompt('Введите кол-во часов для перевода в секунды');
+} while (isInvalid(askHoues));
+
+askHoues = Number(askHoues);
+
+
+function isInvalid(str) {
+    return str === null || str.trim() === '' || isNaN(str);
+}
+
+const result = Number(askHoues) * 3600
+
+
+
+alert(`
+Вы ввели: ${askHoues}ч
+Это: ${result} в секундах
+`);
