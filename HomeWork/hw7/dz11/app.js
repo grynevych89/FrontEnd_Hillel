049b98cc6731b437c1cd7f6029f089998295e070
grynevych89 <grynevych89@gmail.com>
2023-02-26 21:18:19 +0200

    done dz11

--- HomeWork/hw7/dz11/app.js
+++ HomeWork/hw7/dz11/app.js
@@ -79,14 +79,31 @@
 // 4. Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
 // Вихідний рядок та символи для видалення задає користувач.
 
-let list = 'hello world'
-let ask = prompt(`Строка: ${list} \n Введите, что нужно изменить`)
+let askStr, askSymbolToDelete, strFromUserByLowerCase, symbolsToReplace = '';
 
-function doReplace(args) {
-    return list
-        .replace(args)
+do {
+    askStr = prompt('Введите текст из которого нужно будет удалить символы:');
+} while (isInvalid(askStr));
+
+do {
+    askSymbolToDelete = prompt('Введите через запятую символы, которые нужно удалить:');
+} while (isInvalid(askSymbolToDelete));
+
+if (askStr !== null) strFromUserByLowerCase = askStr.toLowerCase();
+if (askSymbolToDelete !== null) symbolsToReplace = askSymbolToDelete.toLowerCase().split(',');
+
+console.log('Вы ввели: ' + askStr)
+console.log(String('Вы ввели на удаление эти символы : ' + symbolsToReplace))
+
+if (typeof askStr === typeof askSymbolToDelete) {
+    console.log(replaceSymbol(strFromUserByLowerCase, symbolsToReplace));
 }
 
-console.log(`Изначальная строка: ${list}
-ВВеденые вами данные для изменения: ${ask}
-Результат: ${doReplace(ask)}`);
+function replaceSymbol(valueToChange, symbols) {
+    symbols.forEach((item) => valueToChange = valueToChange.replaceAll(`${item}`, ''));
+    return valueToChange;
+}
+
+function isInvalid(str) {
+    return str === null || str.trim() === '';
+}
