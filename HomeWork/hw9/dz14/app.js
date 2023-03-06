9fce24791b0d84f54aef2b1e600f6e184100ae92
grynevych89 <grynevych89@gmail.com>
2023-03-06 14:56:21 +0200

    add dz 14

---
+++ HomeWork/hw9/dz14/app.js
@@ -0,0 +1,12 @@
+const shoppingCart = {
+  items: [
+    {
+      name: 'Headphone',
+      count: 1,
+      price: 100
+    }
+  ],
+  addItem(item) {},
+  deleteItem(item) {},
+  getTotalSum(){}
+};
