731dd755666fe37caf965a584a6d8680ce0fdf3b
grynevych89 <grynevych89@gmail.com>
2023-04-08 17:00:04 +0300

    done dz22

---
+++ HomeWork/dz22/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const inputElement = $('input');
+const outputStyle = {
+    'padding':'4px',
+    'margin':'2px',
+    'color':'whitesmoke',
+    'background-color':'#35aadc',
+    'border':'1px solid black',
+    'border-radius':'10px',
+    'font-size':'large',
+    'cursor':'pointer'
+};
+
+$('button').click(function () {
+    const inputValue = inputElement.val();
+    const checkValue = inputValue.trim();
+    (checkValue) ? $('<span></span>').css(outputStyle).text(inputValue).appendTo('.output-container').hide().fadeIn(1500) : alert("Введите какой то текст!");
+    inputElement.val('');
+})
+
+$('.output-container').on('click', 'span', function () {
+    $(this).fadeOut(1500);
+    setTimeout(() => $(this).remove(), 2000);
+})
