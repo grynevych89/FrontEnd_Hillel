9189425dab04bd7dea951d4db9120d2a3c65be14
grynevych89 <grynevych89@gmail.com>
2023-04-09 14:49:50 +0300

    done dz23

--- HomeWork/dz23/app.js
+++ HomeWork/dz23/app.js
@@ -1,6 +1,7 @@
  $(document).ready(function() {
     $('form').submit(function() {
-        $.get('http://api.openweathermap.org/data/2.5/weather?q=' + $('#weather_input').val() + '&units=metric&APPID=5d066958a60d315387d9492393935c19', function(data) {
+        $.get('http://api.openweathermap.org/data/2.5/weather?q=' + $('#weather_input').val()
+            + '&units=metric&APPID=5d066958a60d315387d9492393935c19&lang=uk', function(data) {
             let html_str = "";
             html_str += "<h2>Місто: " + data.name + "</h2>";
             html_str += "<h3>Температура: " + data.main.temp + " °C</h3>";
@@ -9,8 +10,7 @@
             html_str += "<h3>Вологість: " + data.main.humidity + "</h3>";
             html_str += "<h3>Швидкість вітру: " + data.wind.speed + "</h3>";
             html_str += "<h3>Напрям у градусах: " + data.wind.deg + "</h3>";
-            const icon = data.weather[0].icon
-            html_str += `<img src='http://openweathermap.org/img/w/${icon}.png'></img>`;
+            html_str += `<img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'></img>`;
             html_str += "<p>____________________________________________________________</p>";
             $('#weather_loc').append(html_str);
         }, "json");
